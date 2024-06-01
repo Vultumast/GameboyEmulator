3d0c281519f1c014c5c372198761f9a15b7887fa
Vultumast <152120810+Vultumast@users.noreply.github.com>
2024-05-31 20:22:35 -0400

    Update Processor.cs

--- EmulatorGUI/EmulatorLib/Processor.cs
+++ EmulatorGUI/EmulatorLib/Processor.cs
@@ -16,41 +16,17 @@ namespace EmulatorGUI.EmulatorLib
         public static extern void processor_delete(nint pointer);
 
 
-
         nint _cpointer = nint.Zero;
-        private bool disposedValue;
 
         public Processor()
         {
             _cpointer = processor_create();
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-                if (_cpointer != nint.Zero)
-                    processor_delete(_cpointer);
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~Processor()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            if (_cpointer != nint.Zero)
+                processor_delete(_cpointer);
         }
     }
 }
